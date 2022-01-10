d14b01733cc0947474ab626396624fe69730e19d
Hansle Lee <hssarah13@gmail.com>
2022-01-10 18:12:00 +0900

    15889

---
+++ algorithms/acmicpc/15889/15889.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int jp[1000000];
+int sgr[1000000];
+vector<pair<int, int>> v;
+int ans[1000000];
+
+int main() {
+   cin >> n;
+   cout << n;
+  return 0;
+}
